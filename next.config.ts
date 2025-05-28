19c2d565a58eb692121dcd57885aad8569681372
meet-javiya0 <meetjaviya29@gmail.com>
2025-05-29 01:52:52 +0530

    added assetPrefix path in config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     /* config options here */
     basePath: "/readme_genius",
+    assetPrefix: '/readme_genius',
     output: 'export',
     typescript: {
         ignoreBuildErrors: true,
